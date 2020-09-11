f9b3fab8e12b77f08b4509e314b34f945edc4fdf
wenisumardi <wenipatungga12@gmail.com>
2020-09-11 17:36:16 +0700

    Menambahkan Class Anjing

--- index.php
+++ index.php
@@ -18,3 +18,19 @@ class Kucing{
  echo "Bisa Terbang : $Kucing->bisa_terbang <br>";
  echo "Suara : $Kucing->suara <br>";
  echo "<hr>";
+
+ class Anjing{
+ 	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+ }
+ $Anjing = new Anjing;
+ $Anjing->nama = "Molly";
+ $Anjing->jumlah_kaki = "4";
+ $Anjing->bisa_terbang = "tidak";
+ $Anjing->suara = "guk guk";
+
+ echo "Anjing <br>";
+ echo "Nama : $Anjing->nama <br>";
+ echo "Jumlah Kaki : $Anjing->jumlah_kaki <br>";
+ echo "Bisa Terbang : $Anjing->bisa_terbang <br>";
+ echo "Suara : $Anjing->suara <br>";
+ echo "<hr>";
